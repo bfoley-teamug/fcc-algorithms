91f8371dc1533b21902d5a733b5b26f4dc252499
Brian Foley <bfoley-teamug@users.noreply.github.com>
2017-09-28 20:32:00 -0700

    Add files via upload

---
+++ spinal-tap.js
@@ -0,0 +1,10 @@
+//Spinal Tap Case
+
+function spinalCase(str) {
+  var regex = /\s+|_+/g;
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  return str.replace(regex, '-').toLowerCase(); 
+ 
+}
+
+spinalCase('This Is Spinal Tap');
