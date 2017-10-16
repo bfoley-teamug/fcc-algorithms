03428ed5ad12a268d06f0ac0fe1e5ece3d0ee95a
Brian Foley <bfoley-teamug@users.noreply.github.com>
2017-10-15 20:55:32 -0700

    Add files via upload

---
+++ arguments-optional.js
@@ -0,0 +1,20 @@
+//Arguments Optional
+
+  var args = arguments;
+  var a = args[0];
+  var b = args[1];
+  
+function isNum(num) {
+    return Number.isInteger(num);
+  }
+
+  if (isNum(a)) {
+    if (isNum(b)) {
+      return a + b;
+    } else if (!b) {
+      return function(b) {
+        if (isNum(b))
+          return a + b;
+      };
+    }	
+  }
